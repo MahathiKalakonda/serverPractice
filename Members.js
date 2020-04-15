88cd7e2e5bd12acc2d13ae72570b4f5736cb7836
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-04-15 15:30:45 +0530

    added Members file

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 91,
+        name: 'Mahathi',
+        email: 'mahathi@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 92,
+        name: 'Ashley',
+        email: 'ashley@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 93,
+        name: 'Divya',
+        email: 'divya@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
